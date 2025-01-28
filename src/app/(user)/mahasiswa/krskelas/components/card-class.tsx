f05c5ccdf14f140083a9c1339f59a13f580bc4c1
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-01-29 01:16:10 +0700

    add: implement AlertDialog component for confirmation actions in class management

--- src/app/(user)/mahasiswa/krskelas/components/card-class.tsx
+++ src/app/(user)/mahasiswa/krskelas/components/card-class.tsx
@@ -5,6 +5,17 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog-kelas";
 
 export function CardClass() {
   return (
@@ -14,7 +25,25 @@ export function CardClass() {
         <CardDescription>Kelas A</CardDescription>
       </CardHeader>
       <CardFooter className="flex justify-between">
-        <Button variant={"destructive"}>Batal Pilih Kelas</Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <div>
+              <Button variant={"destructive"}>Batal Pilih Kelas</Button>
+            </div>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Apakah kamu yakin ingin menghapus kelas?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Tindakan ini tidak dapat dibatalkan. Ini akan secara permanen menghapus data kelas.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction className="bg-destructive">Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </CardFooter>
     </Card>
   );
