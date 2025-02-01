7775583c04f213b7c2a31404e195fe7094d6f297
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-02 02:23:48 +0700

    feat: enhance data fetching with token validation and redirect on expiration

--- src/lib/fetching-component-get.ts
+++ src/lib/fetching-component-get.ts
@@ -1,16 +1,22 @@
 import url_fetch from "@/constant/data-fetching";
+import { toast } from "@/hooks/use-toast";
 import { GetToken } from "@/lib/get-token";
+import Router from "next/router";
 
 export default async function GetFetchingData(url: string) {
   const token = await GetToken();
 
-  const response = await fetch(`${url_fetch}/${url}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  });
-
-  return response.json();
+  if (!token) {
+    toast({ title: "Sesi anda berakhir", description: "Silahkan login kembali" });
+    Router.push("/auth");
+  } else {
+    const response = await fetch(`${url_fetch}/${url}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  }
 }
