8b2fb5a8af7c9101f1028459d6f3d7e78e3b2884
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-02 02:28:33 +0700

    fix: update redirection method to use NextResponse for session expiration handling

--- src/lib/fetching-component-get.ts
+++ src/lib/fetching-component-get.ts
@@ -1,14 +1,14 @@
 import url_fetch from "@/constant/data-fetching";
 import { toast } from "@/hooks/use-toast";
 import { GetToken } from "@/lib/get-token";
-import Router from "next/router";
 
+import { NextResponse } from "next/server";
 export default async function GetFetchingData(url: string) {
   const token = await GetToken();
 
   if (!token) {
     toast({ title: "Sesi anda berakhir", description: "Silahkan login kembali" });
-    Router.push("/auth");
+    return NextResponse.redirect(new URL("/auth"));
   } else {
     const response = await fetch(`${url_fetch}/${url}`, {
       method: "GET",
