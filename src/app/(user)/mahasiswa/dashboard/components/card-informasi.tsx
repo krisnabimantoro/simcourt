a1d49528494e2d9f53d19a9e4f60e8e4236b2c13
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-03 10:31:53 +0700

    feat: update CardInformasi component with image and enhanced footer content for improved presentation

--- src/app/(user)/mahasiswa/dashboard/components/card-informasi.tsx
+++ src/app/(user)/mahasiswa/dashboard/components/card-informasi.tsx
@@ -1,17 +1,21 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 export default function CardInformasi() {
   return (
-    <Card className="h-full">
+    <Card className={cn("h-full")}>
       <CardHeader>
         <CardTitle>Informasi Simcourt</CardTitle>
         <CardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. </CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        <Image src={"/logohukum.png"} width={260} height={20} alt={""} />
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <p className="font-bold">
+          Ecourt adalah <span className="font-normal">Lorem ipsum dolor sit amet, consectetur adipisicing elit. ?Libero ullam, magnam asperiores facere, omnis corrupti sequi dolore architecto totam dicta error suscipit doloremque, necessitatibus vitae a laborum recusandae. Voluptas, eos</span>
+        </p>
       </CardFooter>
     </Card>
   );
