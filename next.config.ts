fa09b934120e687fb18dd8e65989aa0f6caedd53
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-19 03:35:59 +0700

    add output: "standalone",

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -15,6 +16,7 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  
   async headers() {
     return [
         {
