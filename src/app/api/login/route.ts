f55ec32045a094632df50e63bcc2d24299be7c11
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-19 04:31:00 +0700

    erorr handling

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -8,22 +8,29 @@ export async function POST(req: Request) {
   //   const cookieStore = await cookies();
 
   // const user = data.find((user) => user.nim === nim && user.password === password);
+  try {
+    const response = await fetch(`http://127.0.0.1:8020/api/v1/auth/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ nim, password }),
+    });
 
-  const response = await fetch(`http://127.0.0.1:8020/api/v1/auth/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ nim, password }),
-  });
-
+    
   const data = await response.json();
-  
+
   if (!response.ok) {
     return NextResponse.json({ message: "Login gagal, periksa kembali NIM atau password!" }, { status: 401 });
   }
 
   await createSession(data.token);
 
-  return NextResponse.json({ message: "Login berhasil",}, { status: 200 });
+  
+  } catch (error) {
+    console.error(error);
+  }
+
+
+  return NextResponse.json({ message: "Login berhasil" }, { status: 200 });
 }
 
 // Simpan token di cookies (HttpOnly lebih aman)
