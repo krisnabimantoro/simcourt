7c1fe4cd49e8ee930089f8181d2e48856698d5f9
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-18 22:59:33 +0700

    feat: remove admin login form component and associated page

--- src/app/auth/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "./components/login-form-admin";
-
-export default function admin() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <LoginForm tittleCard={"Login Simcourt Admin"} />
-    </div>
-  );
-}
