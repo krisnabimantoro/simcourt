3a1fc53c087808d37b01b0ee8c56e51ea3a8d217
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-02-20 23:24:41 +0700

    update: change return number

--- src/lib/get-id-user.ts
+++ src/lib/get-id-user.ts
@@ -1,8 +1,9 @@
 import { decrypt } from "@/lib/session";
 import { cookies } from "next/headers";
 
-export async function GetIdUser(){
+export async function GetIdUser() {
   const session = (await cookies()).get("session")?.value;
   const payload = await decrypt(session);
-  return payload?.sub;
+  const userId = Number(payload?.sub);
+  return userId;
 }
