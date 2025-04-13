1810517119e92d731a5ed47bcf8bd921174b8f9d
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-04-13 07:44:13 +0700

    feat: add value prop to InputWithLabelReq for improved input handling

--- src/components/ui/input-with-label-req.tsx
+++ src/components/ui/input-with-label-req.tsx
@@ -6,17 +6,18 @@ interface InputProps {
   placeholder: string;
   name: string;
   type: string;
+  value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function InputWithLabelReq({ label, placeholder, name, type, onChange }: InputProps) {
+export default function InputWithLabelReq({ label, placeholder, name, type, onChange,value }: InputProps) {
   return (
     <div className="space-y-2">
       <Label htmlFor={name}>
         {label}
         <span className="text-destructive">*</span>
       </Label>
-      <Input id={name} placeholder={placeholder} type={type} required name={name} onChange={onChange} />
+      <Input id={name} placeholder={placeholder} type={type} required name={name} onChange={onChange} value={value} />
     </div>
   );
 }
