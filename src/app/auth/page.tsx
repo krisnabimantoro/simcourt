92f69ee78d35e7a87d6dc0ef52e1b19f87e3dca2
Krisna Bimantoro <krisnabmntr@gmail.com>
2025-06-13 23:22:00 +0700

    fix: clean up unused imports and improve layout structure in Login component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,45 +1,50 @@
-// import { Button } from "@/components/ui/button";
-// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
 import Image from "next/image";
 import LoginForm from "./components/login-form";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignUpForm from "./components/signup-form";
 import BlurFade from "@/components/ui/blur-fade";
-// import DotPattern from "@/components/ui/dot-pattern";
-// import { cn } from "@/lib/utils";
 
 export default function Login() {
   return (
-    <div className="h-screen w-screen flex  items-center justify-center  gap</div>-[80px]">
-      <div
-        className="w-full justify-center items-center flex h-screen bg-hero bg-[url(/mahkamah.jpg)] 
-      
-      bg-center "
-      >
-        <div className="absolute w-full h-full bg-white bg-opacity-80"></div>
-        {/* <div className=" w-fit h-fit p-6 bg-white">
-        </div> */}
+    <div className="h-screen w-screen flex flex-col md:flex-row items-center justify-center gap-4">
+      {/* Bagian kiri (disembunyikan di mobile) */}
+      <div className="hidden md:flex w-1/2 justify-center items-center h-screen bg-[url(/mahkamah.jpg)] bg-center bg-cover relative">
+        <div className="absolute w-full h-full bg-white bg-opacity-80 z-0"></div>
         <BlurFade>
-          <Image src="/logo_simucourt.png" width={500} height={500} alt="Logo Laboratorium Hukum" />
+          <Image
+            src="/logo_simucourt.png"
+            width={300}
+            height={300}
+            alt="Logo Laboratorium Hukum"
+            className="md:w-[500px] md:h-[500px] z-10"
+          />
         </BlurFade>
       </div>
-      <div className="w-1/2">
-        <BlurFade delay={0.25}>
-          <Tabs defaultValue="login" className="w-[400px] mx-auto">
-            <TabsList>
-              <TabsTrigger value="login">Login</TabsTrigger>
-              <TabsTrigger value="signup">Buat Akun</TabsTrigger>
-            </TabsList>
-            <TabsContent value="login">
-              <LoginForm />
-            </TabsContent>
-            <TabsContent value="signup">
-              <SignUpForm />
-            </TabsContent>
-          </Tabs>
-        </BlurFade>
+      <div className="flex justify-center items-center flex-col md:flex-row w-full md:w-1/2 h-screen  relative ">
+        <Image
+          src="/logo_simucourt.png"
+          width={300}
+          height={300}
+          alt="Logo Laboratorium Hukum"
+          className="md:w-[500px] md:h-[500px] z-10 md:block lg:hidden "
+        />
+
+        <div className="w-full md:w-1/2 px-4 md:px-0">
+          <BlurFade delay={0.25}>
+            <Tabs defaultValue="login" className="w-full max-w-[400px] mx-auto">
+              <TabsList>
+                <TabsTrigger value="login">Login</TabsTrigger>
+                <TabsTrigger value="signup">Buat Akun</TabsTrigger>
+              </TabsList>
+              <TabsContent value="login">
+                <LoginForm />
+              </TabsContent>
+              <TabsContent value="signup">
+                <SignUpForm />
+              </TabsContent>
+            </Tabs>
+          </BlurFade>
+        </div>
       </div>
     </div>
   );
